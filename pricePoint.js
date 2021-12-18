4f23302de7f55c79d8bad2c1be8b85baa9a8667a
Devin Bidwell <dbidwell94@gmail.com>
2021-12-18 14:41:14 -0800

    Abstract css and JS, initial button styles

---
+++ pricePoint.js
@@ -0,0 +1,27 @@
+const setPrices = [57, 87, 97, 127];
+const sorryText =
+  "We're so sorry! This was a marketing study to determine the most viable price point. Please subscribe to our newsletter to find out when Atlas will officially launch!";
+
+/**
+ * Sets the price point to a random point for analytics purposes
+ * @param {HTMLScriptElement} parent - The HTML Section that hold the HTMLButtonElement
+ */
+function setPricePoint(parent) {
+  if (!parent) {
+    return;
+  }
+
+  const completedPurchase = document.createElement('p');
+  completedPurchase.innerText = sorryText;
+
+  const el = document.createElement('button');
+  el.id = 'purchase-cta';
+  parent.appendChild(el);
+
+  const randomPrice = setPrices[Math.floor(Math.random() * setPrices.length)];
+  el.innerText = `Buy now: $${randomPrice}`;
+  el.onclick = () => {
+    parent.removeChild(el);
+    parent.appendChild(completedPurchase);
+  };
+}
